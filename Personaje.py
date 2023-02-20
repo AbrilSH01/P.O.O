78097f77f78b2a82dfd7cfb60c0158a6260470cf
AbrilSH01 <121037785@upq.edu.mx>
2023-02-20 15:28:05 -0600

    Se creo el método correr

--- Personaje.py
+++ Personaje.py
@@ -3,3 +3,10 @@ class Personaje:
     especie="Humano"
     nombre="MasterChief"
     altura=2.70
+    
+    #Métodos
+    def correr(self,status):
+        if(status):
+            print("El personaje "+ self.nombre + "está corriendo")
+        else:
+            print("El personaje "+self.nombre+ "se detuvo")
