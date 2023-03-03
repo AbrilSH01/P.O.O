78dcf691cfa58469983d1c3dfbb6ca911e07687c
AbrilSH01 <121037785@upq.edu.mx>
2023-03-02 22:25:37 -0600

    Inicio de sesión

---
+++ tkinther/práctica12.py
@@ -0,0 +1,12 @@
+from tkinter import*
+
+ventana=Tk()
+ventana.title("Inicio de sesión")
+ventana.geometry("600x400")
+
+seccion1=Frame(ventana,bg="white")
+seccion1.pack(expand=True,fill='both')
+
+
+
+ventana.mainloop()
