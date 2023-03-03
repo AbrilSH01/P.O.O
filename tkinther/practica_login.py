1776043dfec32bd8f0b196b97e2f119511c08c5d
AbrilSH01 <121037785@upq.edu.mx>
2023-03-03 13:11:35 -0600

    mensaje validacion

--- tkinther/practica_login.py
+++ tkinther/practica_login.py
@@ -1,2 +1,13 @@
 from tkinter import *
 
+correo="121037785@upq.edu.mx"
+password="password1234"
+
+#1. Funcion para el mensaje de advertencia
+
+def validacion():
+    if correo == email.get() and password == contraseña.get():
+        messagebox.showinfo("Bienvenido cardenal!")
+    else:
+        messagebox.showinfo("Error, los datos son incorrectos!")
+        
