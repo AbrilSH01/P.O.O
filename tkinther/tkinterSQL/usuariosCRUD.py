f339af5fa4b0c2f449e42ed004b71ad585144cd9
AbrilSH01 <121037785@upq.edu.mx>
2023-03-22 15:07:52 -0600

    creación de ventana y BD

---
+++ tkinther/tkinterSQL/usuariosCRUD.py
@@ -0,0 +1,11 @@
+from tkinter import*
+from tkinter import ttk
+import tk as tk
+from tkinter import messagebox
+
+
+ventana=Tk()
+ventana.title("BIENVENIDO")
+ventana.geometry("500x300")
+
+ventana.mainloop()
